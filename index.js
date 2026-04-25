f6bc40ce634a2da4307faa0457f076d90fb6d411
saiyancodes57 <256424803+saiyancodes57@users.noreply.github.com>
2026-04-25 22:17:57 +0100

    Added focus trapping and visibility hidden for nav, improving a11y keyboard tab focus

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const mainElement = document.querySelector(".main");
 function closeMenu() {
   menu.classList.remove("header__nav--active");
   navOverlay.classList.remove("header__nav-overlay--active");
+  // Remove focus trap listener when closed
+  document.removeEventListener("keydown", trapFocus);
 }
 
 function openMenu() {
   menu.classList.add("header__nav--active");
   navOverlay.classList.toggle("header__nav-overlay--active");
+  // Add focus trap listener
+  document.addEventListener("keydown", trapFocus);
+
+  // Focus the first link on opening the nav
+  const firstLink = menu.querySelector(".header__list a");
+  firstLink?.focus();
 }
 
 // Button for opening the menu
@@ -31,7 +39,35 @@ window.addEventListener("resize", () => {
   }
 });
 
-// When clicking outside the nav it closes the menu
+// When clicking outside the nav it closes the menu (i.e., on the div.overlay)
 navOverlay.addEventListener("click", () => {
   closeMenu();
 });
+
+// Escape key closes nav menu
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") closeMenu();
+});
+
+function trapFocus(e) {
+  // Get all possible elements that can focus
+  const focusable = menu.querySelectorAll("a, button");
+
+  const first = focusable[0];
+  const last = focusable[focusable.length - 1];
+
+  const isShift = e.shiftKey;
+  const active = document.activeElement;
+
+  //  TAB forward: If not shift and current active element is last element
+  if (!isShift && active === last) {
+    e.preventDefault();
+    first.focus();
+  }
+
+  // SHIFT + TAB backward: but if equal to first, shift focus to last element
+  if (isShift && active === first) {
+    e.preventDefault();
+    last.focus();
+  }
+}
