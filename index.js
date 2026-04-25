0323040edf70415a1fe944606f8d7ba7c307ef1b
saiyancodes57 <256424803+saiyancodes57@users.noreply.github.com>
2026-04-25 20:49:53 +0100

    Added close menu functions

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const menuBtn = document.querySelector(".header__menu-btn");
 const menu = document.querySelector(".header__nav");
-const closeBtn = document.querySelector(".header__nav-close-btn"); // Add this to your HTML
+const closeBtn = document.querySelector(".header__nav-close-btn");
+const navOverlay = document.querySelector(".header__nav-overlay");
+const mainElement = document.querySelector(".main");
 
-menuBtn.addEventListener("click", () => {
+function closeMenu() {
+  menu.classList.remove("header__nav--active");
+  navOverlay.classList.remove("header__nav-overlay--active");
+}
+
+function openMenu() {
   menu.classList.add("header__nav--active");
+  navOverlay.classList.toggle("header__nav-overlay--active");
+}
+
+// Button for opening the menu
+menuBtn.addEventListener("click", () => {
+  openMenu();
 });
 
+// Button for closing the menu
 closeBtn.addEventListener("click", () => {
-  menu.classList.remove("header__nav--active");
+  closeMenu();
+});
+
+// Ensuring the nav closes when resizing to larger screens
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1440) {
+    closeMenu();
+  }
+});
+
+// When clicking outside the nav it closes the menu
+navOverlay.addEventListener("click", () => {
+  closeMenu();
 });
